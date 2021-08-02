47268b1ae07f2d58cf519d1c8011ef524c9a971f
hidde-vb <hiddevanbavel@hotmail.com>
2021-08-02 08:49:04 +0200

    feat: functional contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import emailjs from 'emailjs-com';
+
 import get from 'lodash/get'
 import { Helmet } from 'react-helmet'
 import Img from 'gatsby-image'
@@ -8,6 +10,31 @@ import Layout from '../components/layout'
 import styles from './contact.module.css'
 
 class RootIndex extends React.Component {
+
+  state = {
+    disabled: false,
+    message: 'verstuur bericht'
+  }
+
+  sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_4f1u8bm', 'template_6or4q6s', e.target, 'user_JCR0vlcC7YkydepGLSyMG')
+      .then((result) => {
+        console.log(result.text)
+        this.setState({
+          disabled: true,
+          message: 'Verzonden! Bedankt voor je bericht.'
+        })
+      }, (error) => {
+        console.log(error.text)
+        this.setState({
+          disabled: true,
+          message: 'Er ging iets mis, probeer later opnieuw.'
+        })
+      })
+  }
+
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const contactImage = get(this, 'props.data.contentfulAsset.contactImage')
@@ -21,22 +48,22 @@ class RootIndex extends React.Component {
           <div className={styles.contactWrapper}>
             <Img className={styles.gridImage} alt={pageContent.title} fluid={pageContent.images[0].fluid} />
             <div className={styles.gridTile}>
-              <form id="contact-form" className={styles.form}>
+              <form id="contact-form" className={styles.form} onSubmit={this.sendEmail}>
                 <input type="hidden" name="contact_number" />
                 <div className={styles.group}>
-                  <label for="user_name">Naam</label>
-                  <input type="text" required id="user_name" placeholder="" />
+                  <label htmlFor="user_name">Naam</label>
+                  <input type="text" required name="user_name" placeholder="" />
                 </div>
                 <div className={styles.group}>
-                  <label for="user_mail">Email</label>
-                  <input type="email" required id="user_mail" placeholder="voorbeeld@mail.com" />
+                  <label htmlFor="user_mail">Email</label>
+                  <input type="email" required name="user_mail" placeholder="voorbeeld@mail.com" />
                 </div>
                 <div className={styles.group}>
-                  <label for="message">Bericht (max 300 karakters)</label>
-                  <textarea pattern=".{10,300}" required id="message" placeholder="uw bericht"></textarea>
+                  <label htmlFor="message">Bericht (max 300 karakters)</label>
+                  <textarea required name="message" placeholder="uw bericht" disabled={this.state.disabled}></textarea>
                 </div>
                 <div className={styles.group}>
-                  <input id="submit" type="submit" value="Verstuur bericht" />
+                  <input id="submit" type="submit" value={this.state.message} disabled={this.state.disabled} />
                 </div>
               </form>
             </div>
