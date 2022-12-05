ddaf3d36b3879908b7ca7710c893ef12313d60b1
hiddevb <hiddevanbavel@hotmail.com>
2022-12-05 22:11:42 +0100

    feat (product): styling, pricing

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { graphql, Link } from "gatsby";
 import { Helmet } from "react-helmet";
 import get from "lodash/get";
@@ -9,20 +9,46 @@ import favicon from "../images/favicon.ico";
 
 import styles from "./product.module.css";
 
-// TODO test a simple checkout
-// TODO show the first spec if available
-// DONE render dropdown on and keep selection in state
-//    TODO disable selection when sold out
-//    TODO when everything is disabled. disable button
+const fetchProduct = async (product) => {
+  const response = await fetch(`${process.env.API_URL}/products/${product}`, {
+    method: "GET",
+  });
+  return response.json();
+};
+
+const formatEuro = (price) =>
+  new Intl.NumberFormat("nl-BE", {
+    currency: "EUR",
+    style: "currency",
+  }).format(price / 100);
+
 const ProductTemplate = (props) => {
   const product = get(props, "data.contentfulProduct");
   const contactImage = get(props, "data.contentfulAsset.contactImage");
-  const prices = get(
-    props,
-    "data.contentfulProduct.prices"
-  );
+  const stripeProducts = get(props, "data.contentfulProduct.prices");
 
-  const [selectedSpec, setSelectedSpec] = useState(prices ? prices[0] : {});
+  const [specs, setSpecs] = useState([]);
+  const [selectedSpec, setSelectedSpec] = useState(null);
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    const fetchSpecs = async () => {
+      const data = await Promise.all(stripeProducts.map(fetchProduct));
+
+      setSpecs(data);
+      setSelectedSpec(data[0].price.id);
+    };
+
+    if (stripeProducts) {
+      fetchSpecs();
+    }
+  }, [stripeProducts]);
+
+  useEffect(() => {
+    const spec = specs.find((e) => e.price.id === selectedSpec);
+
+    if (spec) setPrice(formatEuro(spec.price.value));
+  }, [selectedSpec]);
 
   const productImages = [...product.images];
   const mainProductImage = productImages.shift();
@@ -56,24 +82,28 @@ const ProductTemplate = (props) => {
                 __html: product.article.childMarkdownRemark.html,
               }}
             />
-
-            {prices?.length > 0 && (
+            {stripeProducts?.length > 0 && selectedSpec ? (
               <div>
-                <select
-                  name="prices"
-                  id="spec-select"
-                  value={selectedSpec}
-                  onBlur={(e) => setSelectedSpec(e.target.value)}
-                >
-                  {prices.map((spec) => {
-                    return <option value={spec.price}>{spec.name}</option>;
-                  })}
-                </select>
+                <div className={styles.select}>
+                  <select
+                    name="prices"
+                    id="spec-select"
+                    value={selectedSpec}
+                    onChange={(e) => setSelectedSpec(e.target.value)}
+                  >
+                    {specs.map((spec) => (
+                      <option key={spec.price.id} value={spec.price.id}>
+                        {spec.name}
+                      </option>
+                    ))}
+                  </select>
+                  <span class="focus"></span>
+                </div>
+                <div>
+                  <h2 className={styles.price}>{price}</h2>
+                  <Checkout spec={selectedSpec} />
+                </div>
               </div>
-            )}
-            
-            {prices?.length > 0 ? (
-              <Checkout />
             ) : (
               <Link className={styles.link} to={`/contact`}>
                 <button className="button">Bestel hier → </button>
@@ -116,7 +146,7 @@ export const pageQuery = graphql`
           html
         }
       }
-      prices,
+      prices
       images {
         id
         title
