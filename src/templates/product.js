ada17ddac6964806e28a50b45676b8233ee6beb0
hiddevb <hiddevanbavel@hotmail.com>
2023-01-07 11:35:30 +0100

    fix: fix queryparams, product layout

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,32 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { graphql, Link } from "gatsby";
-import { Helmet } from "react-helmet";
-import get from "lodash/get";
-import Img from "gatsby-image";
+import React, { useEffect, useState } from 'react';
+import { graphql, Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
+import get from 'lodash/get';
+import Img from 'gatsby-image';
 
-import Layout from "../components/layout";
-import Checkout from "../components/checkout";
-import favicon from "../images/favicon.ico";
+import Layout from '../components/layout';
+import Checkout from '../components/checkout';
+import favicon from '../images/favicon.ico';
 
-import styles from "./product.module.css";
+import styles from './product.module.css';
 
 const fetchProduct = async (product) => {
   const response = await fetch(`${process.env.API_URL}/products/${product}`, {
-    method: "GET",
+    method: 'GET',
   });
   return response.json();
 };
 
-const formatEuro = (price) =>
-  new Intl.NumberFormat("nl-BE", {
-    currency: "EUR",
-    style: "currency",
+const formatEuro = (price) => {
+  return new Intl.NumberFormat('nl-BE', {
+    currency: 'EUR',
+    style: 'currency',
   }).format(price / 100);
+};
 
 const ProductTemplate = (props) => {
-  const product = get(props, "data.contentfulProduct");
-  const contactImage = get(props, "data.contentfulAsset.contactImage");
-  const stripeProducts = get(props, "data.contentfulProduct.prices");
+  const product = get(props, 'data.contentfulProduct');
+  const contactImage = get(props, 'data.contentfulAsset.contactImage');
+  const stripeProducts = get(props, 'data.contentfulProduct.prices');
 
   const [specs, setSpecs] = useState([]);
   const [selectedSpec, setSelectedSpec] = useState(null);
@@ -53,11 +54,7 @@ const ProductTemplate = (props) => {
   const mainProductImage = productImages.shift();
 
   return (
-    <Layout
-      location={props.location}
-      footerImage={contactImage}
-      hasBigLogo={false}
-    >
+    <Layout location={props.location} footerImage={contactImage} hasBigLogo={false}>
       <Helmet title={`${product.title}`}>
         <link rel="icon" href={favicon} />
       </Helmet>
@@ -67,12 +64,7 @@ const ProductTemplate = (props) => {
         </Link>
         <div className={styles.productWrapper}>
           <div className={styles.gridTileContainer}>
-            <Img
-              className={styles.gridTile}
-              key={mainProductImage.id}
-              alt={mainProductImage.title}
-              fluid={mainProductImage.fluid}
-            />
+            <Img className={styles.gridTile} key={mainProductImage.id} alt={mainProductImage.title} fluid={mainProductImage.fluid} />
           </div>
           <div className={`${styles.gridTile} ${styles.article}`}>
             <h2 className="title">{product.title}</h2>
@@ -84,13 +76,7 @@ const ProductTemplate = (props) => {
             {stripeProducts?.length > 0 && selectedSpec ? (
               <div>
                 <div className={styles.select}>
-                  <select
-                    name="prices"
-                    id="spec-select"
-                    value={selectedSpec}
-                    onChange={(e) => setSelectedSpec(e.target.value)}
-                    onBlur={(e) => setSelectedSpec(e.target.value)}
-                  >
+                  <select name="prices" id="spec-select" value={selectedSpec} onChange={(e) => setSelectedSpec(e.target.value)} onBlur={(e) => setSelectedSpec(e.target.value)}>
                     {specs.map((spec) => (
                       <option key={spec.price.id} value={spec.price.id}>
                         {spec.name}
@@ -111,14 +97,7 @@ const ProductTemplate = (props) => {
             )}
           </div>
           {productImages.map((image) => {
-            return (
-              <Img
-                className={styles.gridTile}
-                key={image.id}
-                alt={image.title}
-                fluid={image.fluid}
-              />
-            );
+            return <Img className={styles.gridTile} key={image.id} alt={image.title} fluid={image.fluid} />;
           })}
         </div>
       </div>
@@ -131,11 +110,7 @@ export default ProductTemplate;
 export const pageQuery = graphql`
   query ProductBySlug($slug: String!) {
     contentfulAsset(title: { eq: "contact" }) {
-      contactImage: fluid(
-        maxWidth: 300
-        maxHeight: 400
-        background: "rgb:000000"
-      ) {
+      contactImage: fluid(maxWidth: 300, maxHeight: 400, background: "rgb:000000") {
         ...GatsbyContentfulFluid_tracedSVG
       }
     }
@@ -150,12 +125,7 @@ export const pageQuery = graphql`
       images {
         id
         title
-        fluid(
-          maxWidth: 600
-          maxHeight: 500
-          quality: 95
-          background: "rgb:000000"
-        ) {
+        fluid(maxWidth: 600, maxHeight: 500, quality: 95, background: "rgb:000000") {
           ...GatsbyContentfulFluid_tracedSVG
         }
       }
