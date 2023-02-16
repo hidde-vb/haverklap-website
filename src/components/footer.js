a909fb491b327d39a28bb5f0a89cc1cdc0ac29a5
hiddevb <hiddevanbavel@hotmail.com>
2023-02-16 22:26:33 +0100

    feat: update btw, and item ordering

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import Img from 'gatsby-image'
-import { Link } from 'gatsby'
+import React from 'react';
+import Img from 'gatsby-image';
+import { Link } from 'gatsby';
 
-import styles from './footer.module.css'
+import styles from './footer.module.css';
 
 export default ({ data }) => (
   <div className={styles.footer}>
     <div className={styles.wrapper}>
-      <Img
-        className={styles.footerImage}
-        fluid={data}
-      />
+      <Img className={styles.footerImage} fluid={data} />
       <div className={styles.row}>
         <ul className={styles.list}>
-          <li><b>Contact</b></li>
+          <li>
+            <b>Contact</b>
+          </li>
           <li>Bieke Van Gils</li>
-          <li><a  href="https://goo.gl/maps/VDhZ7FouEU1Wjpg46" rel="noreferrer" target="_blank">Adres Winkel</a></li>
+          <li>
+            <a href="https://goo.gl/maps/VDhZ7FouEU1Wjpg46" rel="noreferrer" target="_blank">
+              Adres Winkel
+            </a>
+          </li>
           <li>0494 79 17 81</li>
           <li>bieke@haverklapbloemen.be</li>
-          <li>rek. BE88 7360 6537 5741</li>
+          <li>IBAN BE10 9734 4356 8304</li>
         </ul>
         <ul className={styles.list}>
-          <li>BTW BE0654.987.352</li>
+          <li>BTW BE 0795.571.432</li>
         </ul>
       </div>
     </div>
@@ -31,4 +34,4 @@ export default ({ data }) => (
       <a href="http://hidde.cc">Hidde van Bavel</a>
     </div>
   </div>
-)
+);
--- src/pages/verkoop.js
+++ src/pages/verkoop.js
@@ -1,46 +1,48 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
-import { Helmet } from 'react-helmet'
+import React from 'react';
+import { graphql } from 'gatsby';
+import get from 'lodash/get';
+import { Helmet } from 'react-helmet';
 
-import styles from './verkoop.module.css'
-import Layout from '../components/layout'
-import ProductPreview from '../components/product-preview'
-import favicon from '../images/favicon.ico'
+import styles from './verkoop.module.css';
+import Layout from '../components/layout';
+import ProductPreview from '../components/product-preview';
+import favicon from '../images/favicon.ico';
 
-class Verkoop extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const pageContent = get(this, 'props.data.contentfulPageContent')
-    const products = get(this, 'props.data.allContentfulProduct.edges')
-    const contactImage = get(this, 'props.data.contentfulAsset.contactImage')
+const Verkoop = (props) => {
+  const siteTitle = get(props, 'data.site.siteMetadata.title');
+  const pageContent = get(props, 'data.contentfulPageContent');
+  const products = get(props, 'data.allContentfulProduct.edges');
+  const contactImage = get(props, 'data.contentfulAsset.contactImage');
 
-    return (
-      <Layout location={this.props.location} footerImage={contactImage} hasBigLogo={false}>
-        <Helmet title={siteTitle} >
-          <link rel="icon" href={favicon} />
-        </Helmet>
-        <div className="wrapper">
-          <h1 className="pageTitle">{pageContent.title}</h1>
-          <div className="textBlock"
-            dangerouslySetInnerHTML={{
-              __html: pageContent.article.childMarkdownRemark.html,
-            }}
-          />
-          <div className={styles.productList}>
-            {products.map(({ node }) => {
-              return (
-                <ProductPreview key={node.slug} product={node} />
-              )
+  return (
+    <Layout location={props.location} footerImage={contactImage} hasBigLogo={false}>
+      <Helmet title={siteTitle}>
+        <link rel="icon" href={favicon} />
+      </Helmet>
+      <div className="wrapper">
+        <h1 className="pageTitle">{pageContent.title}</h1>
+        <div
+          className="textBlock"
+          dangerouslySetInnerHTML={{
+            __html: pageContent.article.childMarkdownRemark.html,
+          }}
+        />
+        <div className={styles.productList}>
+          {products
+            .sort((a, b) => {
+              if (a.node.order < b.node.order) return 1;
+              return -1;
+            })
+            .map(({ node }) => {
+              return <ProductPreview key={node.slug} product={node} />;
             })}
-          </div>
         </div>
-      </Layout>
-    )
-  }
-}
+      </div>
+    </Layout>
+  );
+};
 
-export default Verkoop
+export default Verkoop;
 
 export const pageQuery = graphql`
   query VerkoopIndexQuery {
@@ -55,15 +57,10 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
-      } 
+      }
     }
     contentfulAsset(title: { eq: "contact" }) {
-      contactImage: fluid(
-        maxWidth: 600
-        maxHeight: 800
-        quality: 90
-        background: "rgb:000000"
-      ) {
+      contactImage: fluid(maxWidth: 600, maxHeight: 800, quality: 90, background: "rgb:000000") {
         ...GatsbyContentfulFluid_tracedSVG
       }
     }
@@ -72,19 +69,15 @@ export const pageQuery = graphql`
         node {
           title
           slug
+          order
           article {
             childMarkdownRemark {
               html
             }
           }
-          images { 
+          images {
             id
-            fluid(
-              maxWidth: 350
-              maxHeight: 400
-              quality: 95
-              background: "rgb:000000"
-            ) {
+            fluid(maxWidth: 350, maxHeight: 400, quality: 95, background: "rgb:000000") {
               ...GatsbyContentfulFluid_tracedSVG
             }
           }
@@ -92,4 +85,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
