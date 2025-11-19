8ff5fac35adcd17850fe3ab47fef7d1c820f274b
hiddevb <hiddevanbavel@hotmail.com>
2025-11-19 12:38:08 +0100

    fix(contact): upgrade emailjs client

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import emailjs from '@emailjs/browser';
 import { graphql } from 'gatsby';
-import emailjs from 'emailjs-com';
-
-import get from 'lodash/get';
-import { Helmet } from 'react-helmet';
 import { GatsbyImage } from 'gatsby-plugin-image';
+import get from 'lodash/get';
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import Layout from '../components/layout';
-
-import * as styles from './contact.module.css';
 import favicon from '../images/favicon.ico';
+import * as styles from './contact.module.css';
 
 class RootIndex extends React.Component {
   state = {
@@ -19,7 +17,11 @@ class RootIndex extends React.Component {
   sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_4f1u8bm', 'template_6or4q6s', e.target, 'user_JCR0vlcC7YkydepGLSyMG').then(
+    emailjs.send('service_4f1u8bm', 'template_6or4q6s', {
+      name: document.querySelector("#user_name").value,
+      message: document.querySelector("#message").value,
+      email: document.querySelector("#user_mail").value,
+    }).then(
       (result) => {
         console.log(result.text);
         this.setState({
@@ -28,7 +30,7 @@ class RootIndex extends React.Component {
         });
       },
       (error) => {
-        console.log(error.text);
+        console.log(error);
         this.setState({
           disabled: true,
           message: 'Er ging iets mis, probeer later opnieuw.',
@@ -41,6 +43,11 @@ class RootIndex extends React.Component {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title');
     const contactImage = get(this, 'props.data.contentfulAsset.gatsbyImage');
     const pageContent = get(this, 'props.data.contentfulPageContent');
+
+    emailjs.init({
+    publicKey: "82LYrTyAJukkNcSym",
+    blockHeadless: true,
+  });
 
     return (
       <Layout location={this.props.location} footerImage={contactImage} hasBigLogo={false}>
